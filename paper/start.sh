b93dbb764be0cce40a3892fe117fef86faf79196
personinblack <berkay@tuta.io>
2020-03-25 05:12:15 +0300

    dockerfile'daki linkleme işlemini start.sh dosyasına taşı
    artık localde bulunan tüm dosyaları otomatik olarak linkliyoruz.
    tek tek dosya ve klasörlerle uğraşmak gerekmiyor. komple plugins
    klasörünü linklemek de yok çünkü içerisinde data dosyaları var.

--- paper/start.sh
+++ paper/start.sh
@@ -1,4 +1,18 @@
+# vim:fileencoding=utf-8:foldmethod=marker:softtabstop=2:shiftwidth=2
+#
 #!/bin/bash
+
+# sunucu config'lerini linkle
+cd $DIR_DATA/config
+find . -type f -exec ln -sf $PWD/{} $DIR_SERVER/{} \;
+
+# plugin jar ve config'lerini linkle
+cd $DIR_DATA/plugins
+find . -type f -exec bash -c 'mkdir $DIR_SERVER/plugins/$(dirname {}) -p &&\
+  ln -sf $PWD/{} $DIR_SERVER/plugins/{}' \;
+
+# sunucuyu başlat
+cd $DIR_SERVER
 java -Dcom.mojang.eula.agree=true -Xms1G -Xmx1G -XX:+UseG1GC\
  -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC\
  -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80\
